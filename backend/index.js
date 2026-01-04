21faf9701fbb8649daa0a985c1afaf5d3985c6e7
shivansu77 <shivansubisht77@gmail.com>
2026-01-04 20:24:04 +0530

    add .gitignore and backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World! india');
+
+});
+
+app.listen(port, () => {
+
+    console.log(`Server is running at http://localhost:${port}`);
+
+});
