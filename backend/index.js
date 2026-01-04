c65adabebb11cb1ae293403c170af8a371a1caa6
shivansu77 <shivansubisht77@gmail.com>
2026-01-04 20:56:17 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+env.config();
+
 const app = express();
-const port = 8000;
 
-app.get('/', (req, res) => {
 
-    res.send('Hello World! india');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
+app.get('/home', (req, res) => {
+    res.json({ message: 'Welcome to the Home Page' });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8000, async() => {
 
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${process.env.PORT || 8000}`);
+    mongoose.connect(process.env.DB_URL).then(() => {
+        console.log('Connected to the database successfully');
+    }).catch((err) => {
+        console.log('Failed to connect to the database', err);
+    });
 
 });
